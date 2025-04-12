2ea3b54e234c5a547117291a65f7c203f000a3a1
XuanHoang1702 <tranxuanhoang17022004@gmail.com>
2025-04-12 21:18:16 +0700

    Update Table and create Procedure

---
+++ Programmability/Procedures/dbo.TASK_List.sql
@@ -0,0 +1,12 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[TASK_List] 
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	select * from TASK
+END
+GO
---
+++ Tables/dbo.TASK.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TASK] (
+  [ID] [int] IDENTITY,
+  [TASK_NAME] [nvarchar](255) NULL,
+  [CREATED_AT] [datetime] NULL DEFAULT (getdate()),
+  [UPDATED_AT] [datetime] NULL DEFAULT (getdate()),
+  [TASK_STATUS] [nvarchar](20) NULL,
+  [MAKED_ID] [nvarchar](20) NOT NULL,
+  [CHECKER_ID] [nvarchar](20) NULL,
+  PRIMARY KEY CLUSTERED ([ID])
+)
+ON [PRIMARY]
+GO
